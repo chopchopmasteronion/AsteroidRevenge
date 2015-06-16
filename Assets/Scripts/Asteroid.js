17554d780728b5381128f316b17c15601ba2651a
flex <flexedoyster@hotmail.com>
2015-06-15 21:47:15 -0400

    Split Logic

--- Assets/Scripts/Asteroid.js
+++ Assets/Scripts/Asteroid.js
@@ -2,15 +2,19 @@
 public class Asteroid extends MonoBehaviour {
 	var size: float;
 	var fasteroid: GameObject;
+	var rigbod: Rigidbody;
+	var splitForce: float;
+	var contact: ContactPoint;
 	
 	function Start () {
-
+		this.gameObject.transform.localScale = new Vector3(size, size, size);
+		rigbod = GetComponent.<Rigidbody>();
 	}
 
 	function Update () {
 		if(this.gameObject.transform.position.y !=10)
 		{
-		stable();
+			stable();
 		}
 	}
 
@@ -19,76 +23,39 @@ public class Asteroid extends MonoBehaviour {
 		this.gameObject.transform.position.y = 10;
 	}
 	
-	function OnCollisionEnter(hit: Collision) {
-		if(hit.gameObject.name == "Laser(Clone)") {
-			split();
-			//SplitMesh();
+	function OnCollisionEnter(hit: Collision){
+		Debug.Log("hit1");
+		contact = hit.contacts[0];
+		Debug.Log("hit2");
+		if(hit.gameObject.name == "Laser(Clone)"){
+			split(contact.point);
 		}
 	}
 	
-	function split() {
-		setSize(1);
+	function split(splitPoint: Vector3) {
+		var astObj: GameObject;
+		decrementSize(1);
+		splitPoint = Quaternion.Euler(0, -90, 0) * splitPoint/10;
+		Debug.Log(splitPoint);
 		if(size > 0.0) {
-			var astPos = this.transform.localPosition;
-			fasteroid.GetComponent(Fasteroid).setSize(size);
-			Instantiate(fasteroid, astPos, transform.localRotation);
+			var astPos = this.transform.localPosition + splitPoint;
+			astObj=Instantiate(fasteroid, astPos, transform.localRotation);
+			astObj.GetComponent(Fasteroid).setSize(size);
+			astObj.GetComponent(Rigidbody).AddForce(splitPoint * splitForce);
+			this.rigbod.AddForce(-splitPoint * splitForce);
 		}
 		else if(size < 0.1) {
 			GameObject.Destroy(this.gameObject);
 		}
 	}
 	
-	function setSize(decrementor:float) {
+	function decrementSize(decrementor:float) {
 		size-=decrementor;
-		this.gameObject.transform.localScale.Set(size, size, size);
+		this.gameObject.transform.localScale = new Vector3(size, size, size);
 	}
 	
-//	function SplitMesh ()
-//     {
-//         var MF = GetComponent(MeshFilter);
-//         var MR = GetComponent(MeshRenderer);
-//         var M = MF.mesh;
-//         var verts = M.vertices;
-//         var normals = M.normals;
-//         var uvs = M.uv;
-//         for (var submesh = 0; submesh < M.subMeshCount; submesh++)
-//         {
-//             var indices = M.GetTriangles(submesh);
-//             for (var i = 0; i < indices.Length; i += 3)
-//             {
-//                 var newVerts = new Vector3[3];
-//                 var newNormals = new Vector3[3];
-//                 var newUvs = new Vector2[3];
-//                 for (var n = 0; n < 3; n++)
-//                 {
-//                     var index = indices[i + n];
-//                     newVerts[n] = verts[index];
-//                     newUvs[n] = uvs[index];
-//                     newNormals[n] = normals[index];
-//                 }
-//                 var mesh: Mesh;
-//                 mesh.vertices = newVerts;
-//                 mesh.normals = newNormals;
-//                 mesh.uv = newUvs;
-//                 
-//                 mesh.triangles = [ 0, 1, 2, 2, 1, 0 ];
-//                 
-//                 var GO = new GameObject("Triangle " + (i / 3));
-//                 GO.transform.position = transform.position;
-//                 GO.transform.rotation = transform.rotation;
-//                 GO.AddComponent(MeshRenderer).material = MR.materials[submesh];
-//                 GO.AddComponent(MeshFilter).mesh = mesh;
-//                 GO.AddComponent(BoxCollider);
-//                 GO.AddComponent(Rigidbody).AddExplosionForce(100, transform.position, 30);
-//                 
-//                 Destroy(GO, 5 + Random.Range(0.0f, 5.0f));
-//             }
-//         }
-//         MR.enabled = false;
-//         
-//         Time.timeScale = 0.2f;
-//         yield WaitForSeconds(0.8f);
-//         Time.timeScale = 1.0f;
-//         Destroy(gameObject);
-//     }
+	function setSize(s: float) {
+		size = s;
+		this.gameObject.transform.localScale = new Vector3(size, size, size);
+	}
 }
--- Assets/Scripts/Fasteroid.js
+++ Assets/Scripts/Fasteroid.js
@@ -1,14 +1,12 @@
 ﻿#pragma strict
 
 public class Fasteroid extends Asteroid {
-	var spawn: Vector3;
-	var dir: Vector3;
-	var rigbod: Rigidbody;
-	
+
 	function Start () {
 	}
-
+	
 	function Update () {
 		this.stable();
 	}
+	
 }
