c8724bfb6611e73f189b8d5c8bb75de58b7bcdc6
MWessel27 <MWessel27@gmail.com>
2016-05-04 07:53:34 -0400

    Converted Asteroid Controls
    Converted Asteroid Controls from JS to C#.

---
+++ Assets/Scripts/Asteroid Scripts/Control.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+
+public class Control : Asteroid
+{
+	int speed;
+	Vector3 moveDirection;
+	Rigidbody rigbod;
+
+	void Start () {
+		rigbod = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		moveDirection.z = Input.GetAxis("Vertical");
+		moveDirection.x = Input.GetAxis("Horizontal");
+		rigbod.AddForce(moveDirection * speed);
+		rigbod.AddTorque(moveDirection * speed);
+	}
+
+	Vector3 getVelocity() {
+		return rigbod.velocity;
+	}
+}
+
+
