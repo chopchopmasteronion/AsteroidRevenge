a152bf744c55c944775ed25e99386c91a6bcc8da
MWessel27 <MWessel27@gmail.com>
2016-05-15 20:39:29 -0400

    Revert "Spawning like asteroid when you go off screen"
    This reverts commit 7f5b281a7f499b5db6bd5e2c994d9318b0d3e104.

--- Assets/Scripts/ScreenBounds.cs
+++ Assets/Scripts/ScreenBounds.cs
@@ -8,43 +8,43 @@ public class ScreenBounds : MonoBehaviour
 	public float topConstraint = 0.0f;
 	public float bottomConstraint = 0.0f;
 	public float buffer = 1.0f; // set this so the spaceship disappears offscreen before re-appearing on other side
-	public float distanceZ = -41.1f; // this is the z-axis of asteroid
+	public float distanceY = 100.0f;
 
 	void Awake() 
 	{
-		// left and right constraints are normal because the x-axis is in fact the x-axis
-		leftConstraint = Camera.main.ScreenToWorldPoint( new Vector3(0.0f, 0.0f, distanceZ) ).x;
-		rightConstraint = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width, 0.0f, distanceZ) ).x;
-		// top and bottom contraints are weird because we are using the z-axis as our y-axis.
-		//	In a normal graph, y-axis would be y-axis. What have we done? (We set the y-axis as the z-axis ;)
-		topConstraint = Camera.main.ScreenToWorldPoint (new Vector3 (0.0f, Screen.height, 0.0f) ).z;
-		bottomConstraint = Camera.main.ScreenToWorldPoint (new Vector3 (0.0f, 0.0f, Screen.height) ).z;
+		// set Vector3 to ( camera left/right limits, spaceship Y, spaceship Z )
+		// this will find a world-space point that is relative to the screen
+
+		// using the camera's position from the origin (world-space Vector3(0,0,0)
+		//leftConstraint = Camera.main.ScreenToWorldPoint( new Vector3(0.0f, 0.0f, 0 - Camera.main.transform.position.z) ).x;
+		//rightConstraint = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width, 0.0f, 0 - Camera.main.transform.position.z) ).x;
+
+		// using a specific distance
+		leftConstraint = Camera.main.ScreenToWorldPoint( new Vector3(0.0f, distanceY, 0.0f) ).x;
+		rightConstraint = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width, distanceY, 0.0f) ).x;
+		topConstraint = Camera.main.ScreenToWorldPoint (new Vector3 (0.0f, Screen.height, distanceY) ).z;
+		bottomConstraint = Camera.main.ScreenToWorldPoint (new Vector3 (0.0f, 0.0f, distanceY) ).z;
+
 	}
 
-	void Update() 
+	void FixedUpdate() 
 	{
-		GameObject astObj = GameObject.Find("Asteroid");
-		var asteroidX = astObj.transform.position.x;
-		var asteroidZ = astObj.transform.position.z;
-
-		if (asteroidX < leftConstraint - buffer) { // ship is past world-space view / off screen
-			asteroidX = rightConstraint + buffer;  // move ship to opposite side
-			astObj.transform.position = new Vector3 (asteroidX, astObj.transform.position.y, astObj.transform.position.z);
-      	}
+		if (this.transform.position.x < leftConstraint - buffer) { // ship is past world-space view / off screen
+			this.transform.position = new Vector3(rightConstraint + buffer, 0.0f, this.transform.position.z);  // move ship to opposite side
+      }
  
-		if (asteroidX > rightConstraint + buffer) {
-			asteroidX = leftConstraint - buffer;
-			astObj.transform.position = new Vector3 (asteroidX, astObj.transform.position.y, astObj.transform.position.z);
-     	}
+      	if (this.transform.position.x > rightConstraint + buffer) {
+			this.transform.position = new Vector3(leftConstraint - buffer, 0.0f, this.transform.position.z);
+      }
 
-		if (asteroidZ > topConstraint + buffer) {
-			asteroidZ = bottomConstraint + buffer;
-			astObj.transform.position = new Vector3 (astObj.transform.position.x, astObj.transform.position.y, asteroidZ);
+		if (this.transform.position.z < bottomConstraint - buffer) { // ship is past world-space view / off screen
+			this.transform.position = new Vector3(this.transform.position.x, 0.0f, topConstraint + buffer);  // move ship to opposite side
 		}
 
-		if (asteroidZ < bottomConstraint + buffer) {
-			asteroidZ = topConstraint + buffer;
-			astObj.transform.position = new Vector3 (astObj.transform.position.x, astObj.transform.position.y, asteroidZ);
+		if (this.transform.position.z > topConstraint + buffer) { // ship is past world-space view / off screen
+			this.transform.position = new Vector3(this.transform.position.x, 0.0f, bottomConstraint - buffer);  // move ship to opposite side
 		}
+
 	}
-}		
+
+}
