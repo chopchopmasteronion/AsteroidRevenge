8960be69b888b489bde28ec737f56fa112ee04da
Mikal Wessel <mwessel27@gmail.com>
2016-05-27 16:48:49 -0400

    Pause Menu
    Full pause menu without ability to pause

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 	public Menu CurrentMenu;
+	bool isPause = false;
 
 	public void Start() {
-		ShowMenu (CurrentMenu);
+		if (CurrentMenu != null) 
+			ShowMenu (CurrentMenu);
 	}
 
 	public void ShowMenu(Menu menu){
@@ -16,11 +18,19 @@ public class MenuManager : MonoBehaviour {
 		CurrentMenu.isOpen = true;
 	}
 
-	public void LoadLevel(){
-		Application.LoadLevel ("debug");
+	public void LoadLevel(string level){
+		Application.LoadLevel (level);
 	}
 
 	public void exitGame() {
 		Application.Quit ();
+	}
+
+	public void pauseGame(bool pauseStatus) {
+		if (pauseStatus == true) {
+			Time.timeScale = 0;
+		} else {
+			Time.timeScale = 1;
+		}
 	}
 }
